1aedcd4cdc14614cf37eccaf1a5d956bcbbdb595
angel-hub31 <moralesmales1@hotmail.com>
2026-05-23 23:15:46 -0500

    se agrega las rutas de scaneo en  ApihibernateApplication

--- src/main/java/com/krakdev/apihibernate/ApihibernateApplication.java
+++ src/main/java/com/krakdev/apihibernate/ApihibernateApplication.java
@@ -2,8 +2,14 @@ package com.krakdev.apihibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.krakdev.peliculas")
+@EnableJpaRepositories(basePackages = "com.krakdev.peliculas.repository")
+@EntityScan(basePackages = "com.krakdev.peliculas.entidades")
 public class ApihibernateApplication {
 
 	public static void main(String[] args) {

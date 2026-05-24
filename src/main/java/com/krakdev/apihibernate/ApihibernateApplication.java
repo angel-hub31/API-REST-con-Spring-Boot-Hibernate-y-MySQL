cbb22dc99e0ec071fab2c4dd1608dbb464eeab78
angel-hub31 <moralesmales1@hotmail.com>
2026-05-23 21:04:07 -0500

    peimer commit

---
+++ src/main/java/com/krakdev/apihibernate/ApihibernateApplication.java
@@ -0,0 +1,13 @@
+package com.krakdev.apihibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihibernateApplication.class, args);
+	}
+
+}

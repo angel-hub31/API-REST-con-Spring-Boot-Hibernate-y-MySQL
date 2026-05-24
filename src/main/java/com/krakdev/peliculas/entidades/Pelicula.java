b2720bce97dde0c51af21fc7b0e21b1460179cca
angel-hub31 <moralesmales1@hotmail.com>
2026-05-23 21:22:13 -0500

    entidad creada, clase Pelicula

---
+++ src/main/java/com/krakdev/peliculas/entidades/Pelicula.java
@@ -0,0 +1,14 @@
+package com.krakdev.peliculas.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+//esto es una entidad
+@Entity
+
+//esto es una tabla de la bdd
+@Table(name = "apihibernate")
+public class Pelicula {
+	
+	
+
+}
